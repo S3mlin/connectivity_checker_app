12167477dfcb99050ccb202b214473e69ba40bc5
S3mlin <lsemic97@gmail.com>
2026-03-12 13:20:16 +0100

    remove dynamodb and add use_lockfile instead

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,8 @@ terraform {
     bucket         = "site-connectivity-checker-terraform-state"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-central-1"
-    dynamodb_table = "terraform-state-locks"
     encrypt        = true
+    use_lockfile   = true
   }
 }
 
@@ -245,17 +245,5 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
-  }
-}
-
-# Create the DynamoDB table for state locking
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
   }
 }
